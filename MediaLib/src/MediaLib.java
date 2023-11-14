af80d0408ce2374d74b93c537464d49cf5b470f5
MatthewLoo1 <142440733+MatthewLoo1@users.noreply.github.com>
2023-11-13 22:16:38 -0800

    Update MediaLib.java

--- MediaLib/src/MediaLib.java
+++ MediaLib/src/MediaLib.java
@@ -1,11 +1,37 @@
 public class MediaLib
 {
   private Book book;
- 
-  public void addBook(Book b)
-  {
-	book = b;
+  private Song song;
+  public static String owner = "PLTW";
+  private static int numEntries;
+
+  //Step 22 and 24
+  public void addBook(Book b) {
+    if (book == null) {
+        book = b;
+        numEntries++;
+    } else {
+        System.out.println("Library already contains a book. Cannot add another book.");
+    }
+}
+//Step 27
+  public void addSong(Song s) {
+    if (song == null) {
+        song = s;
+        numEntries++;
+    } else {
+        System.out.println("Library already contains a book. Cannot add another book.");
+    }
+}
+ // optional step 24
+public void changeBook(Book newBook) {
+  if (book != null) {
+      book = newBook;
+      System.out.println("Book replaced in the library.");
+  } else {
+      System.out.println("No book exists in the library to be replaced.");
   }
+}
    
   // new step 18-22
   /* test scenario
@@ -24,12 +50,23 @@ public class MediaLib
  
 	// Step 5, avoid crashing by testing that book is not null before using its toString method
 	if (book != null)
-  	info += "Book: " + book + "\n";
+  	info += "Book: " + book + "\n" + "Song: " + song;
  
 	return info;
   }
 
-  public static String getOwner() {
-      return "PLTW";
+  //Step 8
+  public void changeOwner(String newOwner) {
+    owner = newOwner;
   }
+
+  public static String getOwner() {
+      return owner;
+  }
+
+  public static int getNumEntries() {
+    //System.out.println("Test: Owner is " + owner);
+    return numEntries;
+  }
+
 }
