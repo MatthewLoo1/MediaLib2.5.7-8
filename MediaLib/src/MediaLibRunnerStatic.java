6b2b39e0d4ae72dca9fa0ab22977a2ac7c3631ac
MatthewLoo1 <142440733+MatthewLoo1@users.noreply.github.com>
2023-11-13 22:17:00 -0800

    Update MediaLibRunnerStatic.java

--- MediaLib/src/MediaLibRunnerStatic.java
+++ MediaLib/src/MediaLibRunnerStatic.java
@@ -1,7 +1,14 @@
+
+//Step 1
+
 public class MediaLibRunnerStatic {
   public static void main(String[] args)
   {
     System.out.println(MediaLib.getOwner() + "'s Library");
+
+    //Step 14
+    System.out.println(MediaLib.getNumEntries());
+    //System.out.println("Test: MediaLib is " + this.book);
   }
   
 }
